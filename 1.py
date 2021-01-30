c71d85b6ffd8aea5f66d7f89380188ec44a04280
zyhgogo <78197122+zyhgogo@users.noreply.github.com>
2021-01-30 18:43:01 +0800

    python
    okk

---
+++ 1.py
@@ -0,0 +1,19 @@
+#字典的合并
+"""a={"rose":"123456","xiaoming":"abc123"}
+b={"lilei":"11111","zhangsan":"123456789"}
+print(a)
+c={**a,**b}
+print(c)"""
+#三元运算符
+"""score=61
+s="pass" if score>60 else "fail"
+print(s)"""
+#序列解包
+"""name="San Zhang"
+first_name,last_name=name.split()
+print(first_name)"""
+#with语句
+#yeild语法
+"""GitHub 账号：zyhgogo
+密码：zhong212upA"""
+
